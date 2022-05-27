f1ea3a34fafcac9cef2349cece8cc1218ff69092
Mark Mikelonis <mark.mikelonis@gmail.com>
2022-05-27 14:18:05 -0700

    travis fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Newer content</p>
+        <p>Newer trigger content</p>
         <a
           className="App-link"
           href="https://reactjs.org"
